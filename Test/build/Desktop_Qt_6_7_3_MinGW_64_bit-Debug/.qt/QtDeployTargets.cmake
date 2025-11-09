dd2d43c8af5dd23a6ca6f3d1eaaa524f19836be6
Xedia <lingxinzhuoxin@qq.com>
2025-11-09 12:18:58 +0800

    Signed-off-by: Xedia <lingxinzhuoxin@qq.com>

--- Test/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Test_FILE E:/Projects/Qt/RogalunaRouteCore/Test/build/debug/bin/Test.exe)
-set(__QT_DEPLOY_TARGET_Test_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Test_RUNTIME_DLLS C:/Program_Files/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Program_Files/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Program_Files/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
